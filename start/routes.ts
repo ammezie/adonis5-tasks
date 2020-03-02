2988794cf288c460ba001e9c839b889c2c7ed184
Chimezie Enyinnaya <meziemichael@gmail.com>
2020-03-02 11:52:42 +0100

    update core

--- start/routes.ts
+++ start/routes.ts
@@ -23,4 +23,4 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', 'TasksController.index')
 Route.post('tasks', 'TasksController.store')
 Route.patch('tasks/:id', 'TasksController.update')
-Route.destroy('tasks/:id', 'TasksController.destroy')
+Route.delete('tasks/:id', 'TasksController.destroy')
